9c29fbc66f5b60d29ab8ad70739f65f11fea0502
Haralishev77 <khorogri@gmail.com>
2024-05-01 21:17:37 +0300

    updated tests

--- test.py
+++ test.py
@@ -4,38 +4,70 @@ from sha3_hash_functions import sha3_224, sha3_256, sha3_384, sha3_512, shake128
 class TestSHA3(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        with open("SHA-3/test_files/Msg_1630bit.md", "r") as file:
-            cls.message = file.read()
-        with open("SHA-3/test_files/True_hashes_1630bit.md", "r") as file:
+        with open("SHA-3/test_files/Messages.md", "r") as file:
+            cls.messages = {}
+            lines = file.read().splitlines()
+            for line in lines:
+                message, length = line.split("|")
+                cls.messages[length] = message
+        with open("SHA-3/test_files/True_hashes.md", "r") as file:
             cls.hashes = {}
             lines = file.read().splitlines()
             for line in lines:
-                hash, name = line.split(";")
-                cls.hashes[name] = hash
+                name, values = line.split("||")
+                cls.hashes[name] = {}
+                elements = values.split("|")
+                for element in elements:
+                    hash, length = element.split(";")
+                    cls.hashes[name][length] = hash
 
     def test_sha3_224(self):
-        true_hash = self.hashes["SHA224"]
-        self.assertEqual(true_hash, sha3_224(self.message).upper())
+        self.assertEqual(self.hashes["SHA224"]["0"], sha3_224(self.messages["0"]).upper())
+        self.assertEqual(self.hashes["SHA224"]["5"], sha3_224(self.messages["5"]).upper())
+        self.assertEqual(self.hashes["SHA224"]["30"], sha3_224(self.messages["30"]).upper())
+        self.assertEqual(self.hashes["SHA224"]["1600"], sha3_224(self.messages["1600"]).upper())
+        self.assertEqual(self.hashes["SHA224"]["1605"], sha3_224(self.messages["1605"]).upper())
+        self.assertEqual(self.hashes["SHA224"]["1630"], sha3_224(self.messages["1630"]).upper())
 
     def test_sha3_256(self):
-        true_hash = self.hashes["SHA256"]
-        self.assertEqual(true_hash, sha3_256(self.message).upper())
+        self.assertEqual(self.hashes["SHA256"]["0"], sha3_256(self.messages["0"]).upper())
+        self.assertEqual(self.hashes["SHA256"]["5"], sha3_256(self.messages["5"]).upper())
+        self.assertEqual(self.hashes["SHA256"]["30"], sha3_256(self.messages["30"]).upper())
+        self.assertEqual(self.hashes["SHA256"]["1600"], sha3_256(self.messages["1600"]).upper())
+        self.assertEqual(self.hashes["SHA256"]["1605"], sha3_256(self.messages["1605"]).upper())
+        self.assertEqual(self.hashes["SHA256"]["1630"], sha3_256(self.messages["1630"]).upper())
 
     def test_sha3_384(self):
-        true_hash = self.hashes["SHA384"]
-        self.assertEqual(true_hash, sha3_384(self.message).upper())
+        self.assertEqual(self.hashes["SHA384"]["0"], sha3_384(self.messages["0"]).upper())
+        self.assertEqual(self.hashes["SHA384"]["5"], sha3_384(self.messages["5"]).upper())
+        self.assertEqual(self.hashes["SHA384"]["30"], sha3_384(self.messages["30"]).upper())
+        self.assertEqual(self.hashes["SHA384"]["1600"], sha3_384(self.messages["1600"]).upper())
+        self.assertEqual(self.hashes["SHA384"]["1605"], sha3_384(self.messages["1605"]).upper())
+        self.assertEqual(self.hashes["SHA384"]["1630"], sha3_384(self.messages["1630"]).upper())
 
     def test_sha3_512(self):
-        true_hash = self.hashes["SHA512"]
-        self.assertEqual(true_hash, sha3_512(self.message).upper())
+        self.assertEqual(self.hashes["SHA512"]["0"], sha3_512(self.messages["0"]).upper())
+        self.assertEqual(self.hashes["SHA512"]["5"], sha3_512(self.messages["5"]).upper())
+        self.assertEqual(self.hashes["SHA512"]["30"], sha3_512(self.messages["30"]).upper())
+        self.assertEqual(self.hashes["SHA512"]["1600"], sha3_512(self.messages["1600"]).upper())
+        self.assertEqual(self.hashes["SHA512"]["1605"], sha3_512(self.messages["1605"]).upper())
+        self.assertEqual(self.hashes["SHA512"]["1630"], sha3_512(self.messages["1630"]).upper())
 
     def test_shake128(self):
-        true_hash = self.hashes["SHAKE128"]
-        self.assertEqual(true_hash, shake128(self.message, 4096).upper())
+        self.assertEqual(self.hashes["SHAKE128"]["0"], shake128(self.messages["0"], 4096).upper())
+        self.assertEqual(self.hashes["SHAKE128"]["5"], shake128(self.messages["5"], 4096).upper())
+        self.assertEqual(self.hashes["SHAKE128"]["30"], shake128(self.messages["30"], 4096).upper())
+        self.assertEqual(self.hashes["SHAKE128"]["1600"], shake128(self.messages["1600"], 4096).upper())
+        self.assertEqual(self.hashes["SHAKE128"]["1605"], shake128(self.messages["1605"], 4096).upper())
+        self.assertEqual(self.hashes["SHAKE128"]["1630"], shake128(self.messages["1630"], 4096).upper())
 
     def test_shake256(self):
-        true_hash = self.hashes["SHAKE256"]
-        self.assertEqual(true_hash, shake256(self.message, 4096).upper())
+        self.assertEqual(self.hashes["SHAKE256"]["0"], shake256(self.messages["0"], 4096).upper())
+        self.assertEqual(self.hashes["SHAKE256"]["5"], shake256(self.messages["5"], 4096).upper())
+        self.assertEqual(self.hashes["SHAKE256"]["30"], shake256(self.messages["30"], 4096).upper())
+        self.assertEqual(self.hashes["SHAKE256"]["1600"], shake256(self.messages["1600"], 4096).upper())
+        self.assertEqual(self.hashes["SHAKE256"]["1605"], shake256(self.messages["1605"], 4096).upper())
+        self.assertEqual(self.hashes["SHAKE256"]["1630"], shake256(self.messages["1630"], 4096).upper())
 
 if __name__ == "__main__":
     unittest.main()
